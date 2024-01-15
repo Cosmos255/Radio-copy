35d5df8f754e7b137d2fd2c75fa4464d3d15f3bf
Cosmos255 <150472944+Cosmos255@users.noreply.github.com>
2024-01-15 17:38:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const number = document.getElementById("number ");
+let numberr;
+const max = 10;
+const min = 0;
+
+
+
+
+window.alert("Salut :) ");
+
+
+function Salut() {
+    number.textContent = numberr = Math.floor(Math.random() * max) + min;
+    console.log("numberr");
+
+    
+
+
+};
